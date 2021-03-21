fc06c2cf9bc28cd579f4747fe600fa4ad669af75
Brady Peterson <bpeterson1663@gmail.com>
2021-03-21 15:28:19 -0500

    feat(nav item): create nav item component

---
+++ src/components/Common/NavItem/NavItem.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useStyles } from './NavItem.styles'
+
+interface NavItemT {
+    width?: number
+    title?: string
+    onClick?: () => void
+}
+export const NavItem: React.FC<NavItemT> = ({children, width, title, onClick}): JSX.Element => {
+    const classes = useStyles()
+    return (
+        <div onClick={onClick} className={classes.container} style={{width: width}}>
+            {title}
+            {children}
+        </div>
+    )
+}
