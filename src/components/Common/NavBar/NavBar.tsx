9e0607589dd4b09dd00c879e766f08fd007b0eea
Brady Peterson <bpeterson1663@gmail.com>
2021-03-21 15:28:00 -0500

    feat(nav bar): create nav bar component

---
+++ src/components/Common/NavBar/NavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useStyles } from './NavBar.styles'
+import { NavItem } from '../NavItem/NavItem'
+import { FaHome } from 'react-icons/fa'
+
+export const NavBar: React.FC = ({children}): JSX.Element => {
+    const classes = useStyles()
+    return (
+        <div className={classes.contaienr}>
+            <div className={classes.iconContainer}>
+                <NavItem width={50}>
+                    <FaHome />
+                </NavItem>
+            </div>
+            <div className={classes.listContainer}>
+                {children}
+            </div>
+        </div>
+    )
+}
+
+ 
---
+++ src/constants/colors.tsx
@@ -0,0 +1,7 @@
+export const Colors = {
+    primary: "#343f3e",
+    secondary: "#505a5b",
+    tertiary: "#94b0da",
+    white: "#fff",
+    navHover: '#495a58'
+}
