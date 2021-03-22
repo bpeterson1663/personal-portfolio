c8dd59149aeffcc1d652c3624e86831dc965ffc7
Brady Peterson <bpeterson1663@gmail.com>
2021-03-21 19:36:08 -0500

    fix(nav item): remove width prop

--- src/components/Common/NavItem/NavItem.tsx
+++ src/components/Common/NavItem/NavItem.tsx
@@ -2,14 +2,13 @@ import React from 'react'
 import { useStyles } from './NavItem.styles'
 
 interface NavItemT {
-  width?: number
   title?: string
   onClick?: () => void
 }
-export const NavItem: React.FC<NavItemT> = ({ children, width, title, onClick }): JSX.Element => {
+export const NavItem: React.FC<NavItemT> = ({ children, title, onClick }): JSX.Element => {
   const classes = useStyles()
   return (
-    <div onClick={onClick} className={classes.container} style={{ width: width }}>
+    <div onClick={onClick} className={classes.container}>
       {title}
       {children}
     </div>
